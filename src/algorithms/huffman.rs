5ce29f1b36395cb28a3b3646dae213bf975ddebc
ranto-dev <rantoandrianandraina@gmail.com>
2026-02-12 12:30:48 +0300

    refactor:remove all rust file

--- src/algorithms/huffman.rs
+++
@@ -1,7 +0,0 @@
-pub fn encode(input: &[u8]) -> Vec<u8> {
-    input.to_vec()
-}
-
-pub fn decode(input: &[u8]) -> Vec<u8> {
-    input.to_vec()
-}
--- src/algorithms/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod bwt_block;
-pub mod lz77;
-pub mod huffman;
